6453902d9845f74562885ca3ba50ace3e0a9f879
Pshubham4 <shubhampadalkar4@gmail.com>
2023-10-01 18:27:09 +0530

    Updated new backend url

--- src/Components/utils/baseURL.js
+++ src/Components/utils/baseURL.js
@@ -1,4 +1,4 @@
-//const baseUrl = "https://blogsite-backend.onrender.com"
-const baseUrl = 'http://192.168.1.205:5050'
+const baseUrl = "https://peakybloggerbackend.onrender.com"
+//const baseUrl = 'http://192.168.1.205:5050'
 
 export default baseUrl
